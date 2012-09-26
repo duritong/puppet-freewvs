4395bfc51373041447ed046925087c9fb56dcc65
o <o@immerda.ch>
2012-09-26 14:47:20 +0200

    code to deploy config in public module, use templates

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,8 @@
 class freewvs(
-  $install_location = '/var/local'
+  $install_location = '/var/local',
+  $repo             = 'git://git.immerda.ch/ifreewvs',
+  $config_src       = 'puppet:///modules/freewvs/freewvs_check.conf',
+  $template_src     = 'puppet:///modules/freewvs/freewvs_check.erb'
 ){
     git::clone{"git_clone_freewvs":
       git_repo => 'git://git.immerda.ch/ifreewvs',
@@ -11,6 +14,16 @@ class freewvs(
       "${install_location}/freewvs_check":
         ensure => file,
         source => 'puppet:///modules/freewvs/freewvs_check',
+        owner => root, group => root, mode => 0700;
+      "/etc/freewvs":
+        ensure => directory;
+      "/etc/freewvs/freewvs_check.conf":
+        ensure => file,
+        source => $config_src,
+        owner => root, group => root, mode => 0700;
+      "/etc/freewvs/freewvs_check.erb":
+        ensure => file,
+        source => $template_src,
         owner => root, group => root, mode => 0700;
     } 
     #File["/etc/cron.d/freewvs_run"]{
