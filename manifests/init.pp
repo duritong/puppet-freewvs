c9fdbc8d920f4d75f4e6e9023dbba36d8fa5dcbd
o <o@immerda.ch>
2012-09-26 15:35:32 +0200

    fix

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,28 +5,28 @@ class freewvs(
   $template_src     = 'puppet:///modules/freewvs/freewvs_check.erb'
 ){
     git::clone{"git_clone_freewvs":
-      git_repo => 'git://git.immerda.ch/ifreewvs',
-      projectroot => "${freewvs::install_location}/freewvs",
-      cloneddir_user => 'root',
+      git_repo        => $repo,
+      projectroot     => "${freewvs::install_location}/freewvs",
+      cloneddir_user  => 'root',
       cloneddir_group => 'root',
     }
     file{
       "${freewvs::install_location}/freewvs_check":
-        ensure => file,
-        source => 'puppet:///modules/freewvs/freewvs_check',
-        owner => root, group => root, mode => 0700;
+        ensure  => file,
+        source  => 'puppet:///modules/freewvs/freewvs_check',
+        owner   => root, group => root, mode => 0700;
       "/etc/freewvs":
-        ensure => directory;
+        ensure  => directory;
       "/etc/freewvs/freewvs_check.conf":
-        ensure => file,
-        source => $freewvs::config_src,
-        owner => root, group => root, mode => 0700;
+        ensure  => file,
+        source  => $freewvs::config_src,
+        owner   => root, group => root, mode => 0700;
       "/etc/freewvs/freewvs_check.erb":
-        ensure => file,
-        source => $freewvs::template_src,
-        owner => root, group => root, mode => 0700;
+        ensure  => file,
+        source  => $freewvs::template_src,
+        owner   => root, group => root, mode => 0700;
     } 
     #File["/etc/cron.d/freewvs_run"]{
-    #  content => "1 * * * * root $install_location/freewvs_check /\n",
+    #  content => "1 * * * * root ${freewvs::install_location}/freewvs_check /\n",
     #}
 }
