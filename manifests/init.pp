897a5d589139f507e993088a41d852ad71059c8f
mh <mh@immerda.ch>
2019-11-27 08:02:28 +0100

    linting

--- manifests/init.pp
+++ manifests/init.pp
@@ -18,25 +18,25 @@ class freewvs(
   }
   file{
     "${install_location}/freewvs_check":
-      ensure  => file,
-      source  => 'puppet:///modules/freewvs/freewvs_check',
-      owner   => root,
-      group   => root,
-      mode    => '0700';
+      ensure => file,
+      source => 'puppet:///modules/freewvs/freewvs_check',
+      owner  => root,
+      group  => root,
+      mode   => '0700';
     '/etc/freewvs':
-      ensure  => directory;
+      ensure => directory;
     '/etc/freewvs/freewvs_check.conf':
-      ensure  => file,
-      source  => $config_src,
-      owner   => root,
-      group   => root,
-      mode    => '0600';
+      ensure => file,
+      source => $config_src,
+      owner  => root,
+      group  => root,
+      mode   => '0600';
     '/etc/freewvs/freewvs_check.erb':
-      ensure  => file,
-      source  => $template_src,
-      owner   => root,
-      group   => root,
-      mode    => '0600';
+      ensure => file,
+      source => $template_src,
+      owner  => root,
+      group  => root,
+      mode   => '0600';
     '/etc/cron.d/freewvs_run':
       content => "14 3 * * * root ${install_location}/freewvs_check\n",
       owner   => root,
